fc705befc248e6213a1a35c8675011175103f1d5
Agnessa14 <agnessakarapetian@gmail.com>
2025-03-14 18:01:10 +0100

    cleaned up

--- EEG/Plotting/plot_decoding.py
+++ EEG/Plotting/plot_decoding.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-PLOTS FOR COMPARISON OF VIDEOS AND STATIC IMAGES
+PLOT DECODING OF IMAGES AND VIDEOS
 
-The plots correspond to figures 3A and 3B in the master's thesis.
-
-@author: AlexanderLenders
+@author: AlexanderLenders, AgnessaKarapetian
 """
 # -----------------------------------------------------------------------------
 # STEP 1: Initialize variables
@@ -16,7 +14,6 @@ if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
 
-    
     # add arguments / inputs
     parser.add_argument('-ls', "--list_sub", default=0, type=int, 
                         metavar='', help="list of subjects")
@@ -40,12 +37,8 @@ if __name__ == "__main__":
                         type = str, metavar='', help="Stats time gen analysis")
     parser.add_argument('-sp', "--save", default = True, type = bool, 
                         metavar='', help="Save plots in SaveDir?")
-    parser.add_argument('-wt', "--wholetg", default = False, type = bool, 
-                        metavar='', help="Plot the whole time gen matrix?")
     parser.add_argument('-f', "--font", default = 'Arial', type = str, 
                         metavar='', help="Font")
-    parser.add_argument('-r', "--redone", default = True, type = bool, 
-                        metavar='', help="Results redone or initial analysis")
     
     args = parser.parse_args() # to get values for the arguments
     
@@ -57,10 +50,7 @@ if __name__ == "__main__":
     wholetg = args.wholetg              # whether to plot the whole time course (-400ms to 980ms) or only a selection, i.e. -200ms to 980ms
     font = args.font                    # which font should be used 
     decoding_dir = args.statsdecoding   # stats of the pairwise scene decoding
-    time_dir = args.timedecoding        # stats of the time gen analysis
     ci_dir = args.cidir                 # directory with the ci's for the decoding analysis
-    redone = args.redone                # whether to load the new (redone) results or old
-
 
 # Subjects included in decoding with videos and decoding with images, respectively:
 list_sub_vid = [6,7,8,9,10,11,17,18,20,21,23,25,27,28,29,30,31,32,34,36]
@@ -81,132 +71,42 @@ plt.rcParams['svg.fonttype'] = 'none'
 # -----------------------------------------------------------------------------
 # STEP 2: Load results (Videos)
 # -----------------------------------------------------------------------------
-
-#Save only non-timegen results (too large to load otherwise) - only need to do this once
-first_time_loading = 0
-
 results_vid = [] # list of dictionaries
 decoding_mean_vid = []
 
-
-if first_time_loading:
-
-    for subject in list_sub_vid: 
-        if subject < 10: 
-            fileDir = (workDir_vid + 
-                    'decoding_time_gen_sub-0{}.npy'.format(subject))
-        else: 
-            fileDir = (workDir_vid + 
-                    'decoding_time_gen_sub-{}.npy'.format(subject))
+for subject in list_sub_vid: 
+    if subject < 10: 
+        fileDir = (workDir_vid + 
+                '/Redone/decoding_miniclips_sub-0{}_redone.npy'.format(subject))
+    else: 
+        fileDir = (workDir_vid + 
+                '/Redone/decoding_miniclips_sub-{}_redone.npy'.format(subject))
         
-        #load all results
-        decoding_results_vid = np.load(fileDir, allow_pickle= True).item()
-        
-        #select only non time gen data
-        notimegen_decoding = {
-        "mean_accuracies_over_conditions": decoding_results_vid["mean_accuracies_over_conditions"],
-        "final_results_mean": decoding_results_vid["final_results_mean"]
-        }
-
-        #clear
-        decoding_results_vid.clear()
-        
-        #save non time gen data
-        if subject < 10: 
-            np.save(os.path.join(workDir_vid,'notimegen_decoding_sub-0{}.npy'.format(subject)),notimegen_decoding)
-        else:
-            np.save(os.path.join(workDir_vid,'notimegen_decoding_sub-{}.npy'.format(subject)),notimegen_decoding)
-
-        print('saved sub ', subject)
-        
-        #put into list with all subjects' data
-        results_vid.append(notimegen_decoding)
-        
-        decoding_accuracy_vid = notimegen_decoding['mean_accuracies_over_conditions']
-        decoding_mean_vid.append(decoding_accuracy_vid)
-    
-        #clear
-        notimegen_decoding.clear()
-
-else:
-    if redone == False:
-        for subject in list_sub_vid: 
-            if subject < 10: 
-                fileDir = (workDir_vid + 
-                        'notimegen_decoding_sub-0{}.npy'.format(subject))
-            else: 
-                fileDir = (workDir_vid + 
-                        'notimegen_decoding_sub-{}.npy'.format(subject))
-            
-            vid_decoding = np.load(fileDir, allow_pickle= True).item()
-            results_vid.append(vid_decoding)
-            print('appended sub', subject)
-            decoding_accuracy_vid = vid_decoding['mean_accuracies_over_conditions']
-            decoding_mean_vid.append(decoding_accuracy_vid)
-    
-    elif redone == True:
-        for subject in list_sub_vid: 
-            if subject < 10: 
-                fileDir = (workDir_vid + 
-                        '/Redone/decoding_miniclips_sub-0{}_redone.npy'.format(subject))
-            else: 
-                fileDir = (workDir_vid + 
-                        '/Redone/decoding_miniclips_sub-{}_redone.npy'.format(subject))
-                
-            vid_decoding = np.load(fileDir, allow_pickle= True).item()
-            results_vid.append(vid_decoding)
-            print('appended sub', subject)
-            decoding_accuracy_vid = vid_decoding['mean_accuracies_over_conditions']
-            decoding_mean_vid.append(decoding_accuracy_vid)
+    vid_decoding = np.load(fileDir, allow_pickle= True).item()
+    results_vid.append(vid_decoding)
+    print('appended sub', subject)
+    decoding_accuracy_vid = vid_decoding['mean_accuracies_over_conditions']
+    decoding_mean_vid.append(decoding_accuracy_vid)
 
 # -----------------------------------------------------------------------------
-# STEP 3: Load results (Static images)
+# STEP 3: Load results (Images)
 # -----------------------------------------------------------------------------
 results_img = [] # list of dictionaries
 decoding_mean_img = []
 timepoints = 70
 plot_timepoints = 60
-num_entries = 16110
-
    
 for subject in list_sub_img: 
-    if redone == False:
-        fileDir = (workDir_img + '/s{}/test/posterior/50hz/pairwise_decoding_timepoints.npy'.format(subject))
-        loaded_data = np.load(fileDir, allow_pickle= True).item()
-        results_img.append(loaded_data)
-        print('appended sub', subject)
-    
-        triangle_mean = np.transpose(loaded_data['rdm_lower'], (2,0,1))
-            
-        final_results_mean = np.zeros((timepoints, num_entries), dtype = float)
-        
-        for tp in range(timepoints): 
-            # only save the lower triangle and vectorize it (excluding main diagonal)
-            # get the indices of the lower triangle, excluding main diagonal 
-            tp_matrix_mean = triangle_mean[tp, :, :]
-            i, j = np.tril_indices_from(tp_matrix_mean, k = -1)
-            lower_triangle_mean = tp_matrix_mean[i, j]
-            vectorized_mean = lower_triangle_mean.flatten()
-            
-            final_results_mean[tp, :] = vectorized_mean
-            
-        # calculate mean decoding accuracy for each timepoint 
-        mean_accuracy = final_results_mean.mean(axis=1)
-        results_img.append(mean_accuracy)
-   
-
+    if subject < 10:
+        fileDir = (workDir_img + '/Redone/decoding_images_sub-0{}_redone.npy'.format(subject))
     else:
-        if subject < 10:
-            fileDir = (workDir_img + '/Redone/decoding_images_sub-0{}_redone.npy'.format(subject))
-        else:
-            fileDir = (workDir_img + '/Redone/decoding_images_sub-{}_redone.npy'.format(subject))
+        fileDir = (workDir_img + '/Redone/decoding_images_sub-{}_redone.npy'.format(subject))
 
-        img_decoding = np.load(fileDir, allow_pickle= True).item()
-        results_img.append(img_decoding)
-        print('appended sub', subject)
-        decoding_accuracy_img = img_decoding['mean_accuracies_over_conditions']
-        decoding_mean_img.append(decoding_accuracy_img)
-
+    img_decoding = np.load(fileDir, allow_pickle= True).item()
+    results_img.append(img_decoding)
+    print('appended sub', subject)
+    decoding_accuracy_img = img_decoding['mean_accuracies_over_conditions']
+    decoding_mean_img.append(decoding_accuracy_img)
 
 # -----------------------------------------------------------------------------
 # STEP 4: Create decoding plot (Figure 3A)
@@ -238,11 +138,9 @@ mean_decoding_diff = mean_decoding_img - mean_decoding_vid
 fileDir_diff = 'difference_decoding_all_subjects.npy'
 np.save(os.path.join(workDir_img,fileDir_diff),mean_decoding_diff)
 
-
 ### 2. CIs ### 
 ##videos
-if redone == True:
-    ci_dir = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/CI_95_accuracy_redone.pkl'
+ci_dir = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/CI_95_accuracy_redone.pkl'
 with open(ci_dir, 'rb') as file:
     confidence_95_vid = pickle.load(file)
 
@@ -253,10 +151,7 @@ high_CI_vid = np.array([confidence_95_vid[key][1] for key in (confidence_95_vid)
 high_CI_vid = high_CI_vid[10:] - chance
 
 ##images
-if redone == False:
-    ci_dir_img = 'Z:/Unreal/Results/Decoding/images/results/CI_95_accuracy.pkl'
-elif redone == True:
-    ci_dir_img = 'Z:/Unreal/Results/Decoding/images/Redone/stats/CI_95_accuracy_redone.pkl'
+ci_dir_img = 'Z:/Unreal/Results/Decoding/images/Redone/stats/CI_95_accuracy_redone.pkl'
    
 with open(ci_dir_img, 'rb') as file:
     img_confidence_95 = pickle.load(file)
@@ -268,10 +163,7 @@ high_CI_img = np.array([img_confidence_95[key][1] for key in (img_confidence_95)
 high_CI_img = high_CI_img[10:] - chance
 
 ##diff 
-if redone == False:
-    ci_dir_diff = 'Z:/Unreal/Results/Decoding/miniclips/stats/diff_CI_95_accuracy.pkl'
-elif redone == True:
-    ci_dir_diff = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/diff_CI_95_accuracy_redone.pkl'
+ci_dir_diff = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/diff_CI_95_accuracy_redone.pkl'
 with open(ci_dir_diff, 'rb') as file:
     confidence_95_diff = pickle.load(file)   
 
@@ -283,8 +175,7 @@ high_CI_diff = high_CI_diff[10:]
 
 ### 3. Stats ###
 ##vid
-if redone == True:
-    decoding_dir = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/decoding_stats_both_nonstd.pkl'
+decoding_dir = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/decoding_stats_both_nonstd.pkl'
 with open(decoding_dir, 'rb') as file:
     decoding_stats_vid = pickle.load(file)
 bool_decoding_vid = decoding_stats_vid['Boolean_statistical_map']
@@ -292,10 +183,7 @@ bool_decoding_vid = bool_decoding_vid[10:]
 array_vid = np.full((plot_timepoints, 1), 0.33)
 
 ##img
-if redone == False:
-    decoding_stats_dir_img = 'Z:/Unreal/Results/Decoding/images/results/decoding_stats_both.pkl'
-elif redone == True:
-    decoding_stats_dir_img = 'Z:/Unreal/Results/Decoding/images/Redone/stats/decoding_stats_both_nonstd.pkl'
+decoding_stats_dir_img = 'Z:/Unreal/Results/Decoding/images/Redone/stats/decoding_stats_both_nonstd.pkl'
 with open(decoding_stats_dir_img, 'rb') as file:
     decoding_stats_img = pickle.load(file)
 bool_decoding_img = decoding_stats_img['Boolean_statistical_map']
@@ -303,11 +191,7 @@ bool_decoding_img = bool_decoding_img[10:]
 array_img = np.full((plot_timepoints, 1), 0.32)
 
 ##diff
-if redone == False:
-    decoding_stats_dir_diff = 'Z:/Unreal/Results/Decoding/miniclips/stats/diff_decoding_stats_both.pkl'
-elif redone == True:
-    decoding_stats_dir_diff = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/diff_decoding_stats_both_nonstd.pkl'
- 
+decoding_stats_dir_diff = 'Z:/Unreal/Results/Decoding/miniclips/Redone/stats/diff_decoding_stats_both_nonstd.pkl'
 with open(decoding_stats_dir_diff, 'rb') as file:
     decoding_stats_diff = pickle.load(file)
     
@@ -316,9 +200,6 @@ bool_decoding_diff = bool_decoding_diff[10:]
 array_diff = np.full((plot_timepoints, 1), 0.31)
 
 ### 4. Plot ###
-
-# fig = plt.subplots(figsize=(8,4.3))
-
 
 ##vid
 color_vid = 'purple'
@@ -355,7 +236,6 @@ plt.plot([np.argmax(mean_decoding_img_minus_chance),np.argmax(mean_decoding_img_
             color='black', alpha=0.6, linestyle=':', linewidth=2)
 plt.show()
 
-
 ##diff
 color_diff = 'black'
 plt.plot(mean_decoding_diff, color=color_diff, linewidth = 2, label = 'Images minus videos')
@@ -368,10 +248,7 @@ plt.show()
 plt.axvline(x=10, color='lightgrey', linestyle='solid', linewidth=2, alpha=0.5, zorder = 0)
 plt.axhline(y=0, color='lightgrey', linestyle='solid', linewidth=2, alpha=0.5, zorder = 0)
 
-
-
 ### Plot params ###
-
 #ticks, labels and legend
 x_ticks_plot = np.arange(0,70,10)
 x_labels_plot = np.arange(-200,1200,200)
